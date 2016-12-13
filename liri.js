ec44c586978827075022e8a7bf4020b662134bac
victoria cortez <victoria cortez>
2016-12-13 18:31:15 -0500

    fixed spotify

--- liri.js
+++ liri.js
@@ -1,8 +1,40 @@
-var request = require('request');
+var key = require('./keys.js');
+// var key = key.twitterKeys
+var twitnpm = require('twitter');
 
-var params = {screen_name: 'victori98770047'};
-request('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
+
+
+var client = new twitnpm ({
+        consumer_key: key.twitterKeys.consumer_key,
+        consumer_secret: key.twitterKeys.consumer_secret,
+        access_token_key: key.twitterKeys.access_token_key,
+        access_token_secret: key.twitterKeys.access_token_secret
+    });
+
+
+
+
+
+var spotify = require('spotify');
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Rick Astley - Never Gonna Give You Up ' + err);
+        return;
+    } 
 });
+
+//commands
+var commands = process.argv[2];
+if (commands == 'my-tweets') {
+	var params = {screen_name: 'Victori98770047'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	   	for (var i = 0; i < tweets.length; i++) {
+	   		console.log(tweets[i].text)
+	   	}
+	    
+	  }
+	});
+}
+	
