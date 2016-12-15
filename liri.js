39eaf56781316311aeca6400f54d396a76e46805
victoria cortez <victoria cortez>
2016-12-15 18:06:10 -0500

    adding mr nobody

--- liri.js
+++ liri.js
@@ -1,84 +1,78 @@
 var key = require('./keys.js');
 // var key = key.twitterKeys
+// ndm requires
 var twitnpm = require('twitter');
+var request = require('request');
 
+// twitter key
 var client = new twitnpm ({
-        consumer_key: key.twitterKeys.consumer_key,
-        consumer_secret: key.twitterKeys.consumer_secret,
-        access_token_key: key.twitterKeys.access_token_key,
-        access_token_secret: key.twitterKeys.access_token_secret,
-    });
-
-//commands twitter stuff
-var commands = process.argv[2];
-
-if (commands == 'my-tweets') {
-	var params = {screen_name: 'Victori98770047'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-	  if (error) throw error
-	  if (!error) {
-	   	for (var i = 0; i < tweets.length; i++) {
-	   		console.log(tweets[i].text)
-	   	}
-	    
-	  } else{
-	  	console.log('broken');
-	  }
-	});
-}
-
-//spotify
-var spotify = require('spotify');
-var song = process.argv[2]
-var url = "https://api.spotify.com/v1/search?q=" + song + "&type=track";
-
-
- 
-spotify.search(url, function(err, response, body) {
-	if (song == 'spotify-this-song') {
-    if ( !err ) {
-        console.log(JSON.parse(body).tracks.items[0].album.name)
-    }else{
-    	console.log('broken')
-    }
- }
-    // DO SOMETHING!!!!!!!! 
+    consumer_key: key.twitterKeys.consumer_key,
+    consumer_secret: key.twitterKeys.consumer_secret,
+    access_token_key: key.twitterKeys.access_token_key,
+    access_token_secret: key.twitterKeys.access_token_secret,
 });
 
-//movie stuff
-var request = require('request');
-var name = process.argv[3];
-//mr nobody url
+// commands
+var commands = process.argv[2];
+// name
+var name = "";
+for (var i = 3; i < process.argv.length; i++) {
+    name += process.argv[i] + " ";
+}
+// movie searches
 var nobody = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json&tomatoes=true";
-//ombi url for every call i need
 var url = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&r=json&tomatoes=true";
 
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log("Title: " + JSON.parse(body).Title);
-    console.log("Year: " + JSON.parse(body).Plot);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Country: " + JSON.parse(body).Country);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-    console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
-  }
- else{
-  request(nobody, function(error, response,body){
-    console.log("Title: " + JSON.parse(body).Title);
-    console.log("Year: " + JSON.parse(body).Plot);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Country: " + JSON.parse(body).Country);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-    console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
- 
+// command lines
+if (commands == 'my-tweets') {
+    var params = {screen_name: 'Victori98770047'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+            }
+        }
+        else {
+            console.log('broken');
+        }
+    });
+}
 
-  	});
-  };
-});
+else if (commands == 'movie-this' && name) {
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+        }
+        else {
+            console.log('broken');
+        }
+    });
+}
+
+else if (commands == 'movie-this' && name == "") {
+    request(nobody, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+        }
+        else {
+            console.log('broken');
+        }
+    });
+}
 
 //fs
 var fs = require('fs');
